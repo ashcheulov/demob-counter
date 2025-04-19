82a0f255a910d9edc62775bf251c2f6139f0108d
ashcheulov <ksnbnfire@gmail.com>
2025-04-19 17:24:57 +0400

    0.0015

--- pip/bot.py
+++ pip/bot.py
@@ -53,7 +53,7 @@ async def start_handler(message: Message):
                 [
                     InlineKeyboardButton(
                         text="Открыть счётчик дембеля (в личке)",
-                        url=f"https://t.me/{bot_username}"
+                        url=f"https://t.me/demobilzationbot"
                     )
                 ]
             ]
